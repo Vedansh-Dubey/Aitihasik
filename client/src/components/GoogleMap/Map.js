09c898a49761ee29d481982cee2785e3eb65b324
Pratik Pimpalkar <pratikpimpalkar21@gmail.com>
2022-07-16 20:29:00 +0530

    Map added

--- client/src/components/GoogleMap/Map.js
+++ client/src/components/GoogleMap/Map.js
@@ -1,21 +1,70 @@
 import React from 'react'
 import { Chart } from "react-google-charts";
 
+const data = [
+    ['State Code', 'State'],
+    ['IN-UP', 'Uttar Pradesh'],
+    ['IN-MH', 'Maharashtra'],
+    ['IN-BR', 'Bihar'],
+    ['IN-WB', 'West Bengal'],
+    ['IN-MP', 'Madhya Pradesh'],
+    ['IN-TN', 'Tamil Nadu'],
+    ['IN-RJ', 'Rajasthan'],
+    ['IN-KA', 'Karnataka'],
+    ['IN-GJ', 'Gujarat'],
+    ['IN-AP', 'Andhra Pradesh'],
+    ['IN-OR', 'Orissa'],
+    ['IN-TG', 'Telangana'],
+    ['IN-KL', 'Kerala'],
+    ['IN-JH', 'Jharkhand'],
+    ['IN-AS', 'Assam'],
+    ['IN-PB', 'Punjab'],
+    ['IN-CT', 'Chhattisgarh',],
+    ['IN-HR', 'Haryana',],
+    ['IN-JK', 'Jammu and Kashmir',],
+    ['IN-UT', 'Uttarakhand',],
+    ['IN-HP', 'Himachal Pradesh',],
+    ['IN-TR', 'Tripura',],
+    ['IN-ML', 'Meghalaya',],
+    ['IN-MN', 'Manipur',],
+    ['IN-NL', 'Nagaland',],
+    ['IN-GA', 'Goa',],
+    ['IN-AR', 'Arunachal Pradesh',],
+    ['IN-MZ', 'Mizoram',],
+    ['IN-SK', 'Sikkim',],
+    ['IN-DL', 'Delhi',],
+    ['IN-PY', 'Puducherry',],
+    ['IN-CH', 'Chandigarh',],
+    ['IN-AN', 'Andaman and Nicobar Islands',],
+    ['IN-DN', 'Dadra and Nagar Haveli',],
+    ['IN-DD', 'Daman and Diu',],
+    ['IN-LD', 'Lakshadweep',]
+]
 
+export const options = {
+    region: 'IN',
+    domain: 'IN',
+    displayMode: 'regions',
+    colorAxis: { colors: ['#e5ef88', '#d4b114', '#e85a03'] },
+    colors: ['red', 'green', 'orange', 'blue', 'yellow'],
+    resolution: 'provinces',
+    backgroundColor: { fill: 'transparent', stroke: "#fff", strokeWidth: "5" },
+    /*datalessRegionColor: '#81d4fa',*/
+    defaultColor: 'cornsilk',
+    // defaultColor: '#ffb071',
+    datalessRegionColor: 'transparent',
+    width: "auto",
+    height: "720",
+};
 const Map = () => {
     return (
         <div>
             <Chart
                 chartType="GeoChart"
-                data={[["Country", "Popularity"],
-                ["Germany", 200],
-                ["United States", 300],
-                ["Brazil", 400],
-                ["Canada", 500],
-                ["France", 600],
-                ["RU", 700],]}
+                data={data}
+                options={options}
                 width="100%"
-                height="400px"
+                height="100%"
                 legendToggle
             />
         </div>
--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -1,11 +1,19 @@
+import { Grid, Typography } from '@mui/material'
 import React from 'react'
 import Map from '../components/GoogleMap/Map'
 
 const Homepage = () => {
   return (
-    <div>
-      <Map/>
-    </div>
+    <Grid container maxWidth={'xl'} justifyContent={'center'}>
+      <Grid item md={8} xs={12}>
+        <div style={{ minHeight: "100vh" }}>
+          <Map />
+        </div>
+      </Grid>
+      <Grid item md={4} xs={12} >
+        <Typography variant="h3" marginTop={3} textAlign={'center'}>Aitihasik</Typography>
+      </Grid>
+    </Grid>
   )
 }
 
